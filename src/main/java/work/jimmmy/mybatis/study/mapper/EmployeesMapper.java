aeaf8104376f8738b7242e2f5b8bd59ba701b61c
shenzui5233 <zui.shen@outlook.com>
2022-07-10 22:49:09 +0800

    (1) 动态sql标签验证：if，foreach (2) pagehelper插件验证

--- src/main/java/work/jimmmy/mybatis/study/mapper/EmployeesMapper.java
+++ src/main/java/work/jimmmy/mybatis/study/mapper/EmployeesMapper.java
@@ -1,9 +1,14 @@
 package work.jimmmy.mybatis.study.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import work.jimmmy.mybatis.study.model.Employee;
 
 import java.util.List;
 
 public interface EmployeesMapper {
-    List<Employee> queryAll();
+    List<Employee> queryEmployeeByCond(@Param("employee") Employee condition, @Param("limit") Integer limit);
+
+    List<Employee> queryEmployeeByIds(@Param("ids") List<Integer> ids, @Param("limit") Integer limit);
+
+    List<Employee> queryEmployeeByPage();
 }
--- src/test/java/work/jimmmy/mybatis/study/BasicTest.java
+++ src/test/java/work/jimmmy/mybatis/study/BasicTest.java
@@ -1,5 +1,7 @@
 package work.jimmmy.mybatis.study;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -10,6 +12,7 @@ import work.jimmmy.mybatis.study.model.Employee;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 
 public class BasicTest {
@@ -19,7 +22,48 @@ public class BasicTest {
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
         SqlSession session = factory.openSession();
         EmployeesMapper mapper = session.getMapper(EmployeesMapper.class);
-        List<Employee> employees = mapper.queryAll();
+        Employee employee = new Employee();
+        employee.setFirstName("Georgi");
+        List<Employee> employees = mapper.queryEmployeeByCond(employee, 100);
         System.out.println(employees);
+        session.close();
+    }
+
+    @Test
+    public void testQueryByIds() throws IOException {
+        InputStream is = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession session = factory.openSession();
+        EmployeesMapper mapper = session.getMapper(EmployeesMapper.class);
+        // findByIds
+        List<Employee> employees = mapper.queryEmployeeByIds(Arrays.asList(10001, 10002, 10003, 10009), 100);
+        System.out.println(employees);
+        session.close();
+    }
+
+    @Test
+    public void testQueryByPage() throws IOException {
+        InputStream is = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession session = factory.openSession();
+        EmployeesMapper mapper = session.getMapper(EmployeesMapper.class);
+        // pageNum从1开始
+        PageHelper.startPage(1, 100);
+        // findByIds
+        List<Employee> employees = mapper.queryEmployeeByPage();
+        System.out.println(employees);
+
+        PageInfo<Employee> pageInfo = new PageInfo<>(employees);
+
+        System.out.println("当前页：" + pageInfo.getPageNum());
+        System.out.println("页大小：" + pageInfo.getPageSize());
+        System.out.println("总记录数：" + pageInfo.getTotal());
+        System.out.println("总页数：" + pageInfo.getPages());
+        System.out.println("上一页：" + pageInfo.getPrePage());
+        System.out.println("下一页：" + pageInfo.getNextPage());
+        System.out.println("是否是第一页：" + pageInfo.isIsFirstPage());
+        System.out.println("是否时最后一页：" + pageInfo.isIsLastPage());
+
+        session.close();
     }
 }
